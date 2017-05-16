7a6d42d37c3204363530801cd93761d814014f94
Alessandro Merola <alessandro.mer.89@gmail.com>
2017-05-16 09:51:47 +0200

    Added my staircase solution

---
+++ staircase/staircase.c
@@ -0,0 +1,30 @@
+/*
+ * This is my solution to the problem available here: https://www.hackerrank.com/challenges/staircase
+ */
+
+# include <stdio.h>
+
+int main(void) {
+	int n;
+	int i;
+	int j = 0;
+
+	printf("Enter a positive integer: ");
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++) {
+		while(j < n - i) {
+			putchar(' ');
+			j++;
+		}
+		j = 0;
+		while(j < i) {
+			putchar('#');
+			j++;
+		}
+		putchar('\n');
+		j = 0;
+	}
+
+	return 0;
+}
